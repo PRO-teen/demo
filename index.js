92137bcb82da8702699f5742cedf4dda7f43c957
PRO-teen <ayushyadav5867@gmail.com>
2025-12-26 16:37:26 +0530

    fix cors

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
+
+
+
 
 // Test route
 app.get("/ping", (req, res) => {
